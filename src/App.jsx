bdfc9e558e9c6b4ceac22606d8534c840ecf07a0
JoseG2003-g <jjoseggg2001@gmail.com>
2026-07-22 03:32:09 +0000

    Added Liked feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,33 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-const App = () =>{
+const Card = ({title}) =>{
+
+  const[hasLiked, setHasLiked] = useState(false);
   return (
-    <h2> Functional Arror Component</h2>
+    <div className="card">
+      <h2> {title} </h2>
+      <button onClick = {() => setHasLiked(true)}> 
+        {hasLiked ? 'Liked' : 'Like'}
+
+      </button>
+    </div>
+  )
+}
+
+const App = () =>{
+
+  
+
+  return (
+    <div className= "card-container">
+      
+      <Card title = "Star Wars" rating ={6} isCool ={true} actors= {[{name: 'Actors'}]}/>
+      <Card title = "Avatar"/>
+      <Card title = "The Lion King"/>
+      
+
+    </div>
   )
 }
 
