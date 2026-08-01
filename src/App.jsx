a75c6b6962301838cc067a1102db433dcb93e26f
JoseG2003-g <jjoseggg2001@gmail.com>
2026-08-01 03:41:24 +0000

    Added appwrite trending backend

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,9 @@ const App = () => {
       }
       setMovieList(data.results || []);
 
-      updateSearchCount();
+      if(query && data.results.length > 0){
+        await updateSearchCount(query, data.results[0]);
+      }
 
     }catch (error){
       console.error(`Error fetching movies ${error}`);
--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import { Databases } from "appwrite";
+import { Databases, ID, Query, Client } from "appwrite";
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
@@ -14,5 +14,26 @@ const databse = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
     //Use appwriet to check if term exists
+    try{
+        const result = await databse.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal('searchTerm', searchTerm),])
+        if(result.documents.length > 0){
+            const doc = result.documents[0];
+
+            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+                count: doc.count + 1,
+            })
+
+            //If it doesnt then create new document
+        }else{
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url: `http://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            })
+        }
+    }catch(error){
+
+    }
 }
 
